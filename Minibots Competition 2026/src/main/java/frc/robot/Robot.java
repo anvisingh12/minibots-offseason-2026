d53967d36af0a3fc4af51e5b0d4ee696ae831629
Anvi Singh <anvi3rcs@gmail.com>
2026-06-20 10:33:13 -0700

    added robot mode and types, I believe

--- Minibots Competition 2026/src/main/java/frc/robot/Robot.java
+++ Minibots Competition 2026/src/main/java/frc/robot/Robot.java
@@ -5,12 +5,16 @@
 package frc.robot;
 
 import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedPowerDistribution;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -36,6 +40,54 @@ public class Robot extends LoggedRobot {
   }
 
   Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
+
+  // Record metadata
+        Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+        Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+        Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+        Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+        Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+        Logger.recordMetadata("Robot", Constants.getRobot().toString());
+        Logger.recordMetadata("Mode", Constants.getMode().toString());
+        Logger.recordMetadata("GitDirty", switch (BuildConstants.DIRTY) {
+          case 0 -> "All changes committed";
+          case 1 -> "Uncommitted changes";
+          default -> "Unknown";
+        });
+        
+        // Set up data receivers & replay source
+        switch (Constants.getMode()) {
+            case REAL:
+                // Running on a real robot, log to a USB stick ("/U/logs")
+                Logger.addDataReceiver(new WPILOGWriter());
+                Logger.addDataReceiver(new NT4Publisher());
+
+                // Setup PDP Logging
+                LoggedPowerDistribution.getInstance(1, ModuleType.kRev);
+
+                break;
+            
+            case SIM:
+                // Running a physics simulator, log to NT
+                Logger.addDataReceiver(new NT4Publisher());
+                Logger.addDataReceiver(new WPILOGWriter());
+                
+                // Silence Joystick Warnings
+                DriverStation.silenceJoystickConnectionWarning(RobotBase.isSimulation());
+                break;
+            
+            case REPLAY:
+                // Replaying a log, set up replay source
+                setUseTiming(false); // Run as fast as possible
+                String logPath = LogFileUtil.findReplayLog();
+                Logger.setReplaySource(new WPILOGReader(logPath));
+                Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_replay")));
+                break;
+        }
+        
+        // Start AdvantageKit
+        Logger.start();
+        
     }
 
   @Override
